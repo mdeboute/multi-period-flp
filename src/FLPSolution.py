505b1af99f806e44af094f241cca71b1e6f624b5
mdeboute <martin.deboute@gmail.com>
2022-11-07 16:46:56 +0100

    small refactoring

--- src/FLPSolution.py
+++ src/FLPSolution.py
@@ -24,7 +24,7 @@ class FLPSolution:
     def __repr__(self):
         return self.__str__()
 
-    def getSitesPeriod(self):
+    def __getSitesPeriod__(self):
         sitesPeriod = [0 for _ in range(self.instance.I)]
         for i in range(self.instance.I):
             for t in range(self.instance.T):
@@ -33,7 +33,7 @@ class FLPSolution:
                     break
         return sitesPeriod
 
-    def getCustomersPeriod(self):
+    def __getCustomersPeriod__(self):
         # we want to know from which period each customer is covered
         # example: [2, 1, 1, 2] means that customer 0 is covered from period 2, customer 1 from period 1, customer 2 from period 1 and customer 3 from period 2
         customersPeriod = [0 for _ in range(self.instance.J)]
@@ -49,11 +49,11 @@ class FLPSolution:
             # a line for each site with the site number (starting at 1) and the period from which the site is open (0 if it is not open)
             # a line for each customer (starting at 1) and the period from which the customer is covered
             file.write(str(self.objective_value))
-            sitesPeriod = self.getSitesPeriod()
+            sitesPeriod = self.__getSitesPeriod__()
             for i in range(self.instance.I):
                 file.write(f"\n{i + 1} {sitesPeriod[i]}")
 
-            customersPeriod = self.getCustomersPeriod()
+            customersPeriod = self.__getCustomersPeriod__()
             for j in range(self.instance.J):
                 file.write(f"\n{j + 1} {customersPeriod[j]}")
 
